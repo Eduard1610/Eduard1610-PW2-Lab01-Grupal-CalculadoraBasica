d1f427ab89bc042fba173164730b13fd0b7bc3e0
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-04-29 18:07:51 -0500

    Se agregó plantilla de la calculadora

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    public static void main (String[] args) {
+
+    }
+	
+    public static int add(int a, int b){
+	return 0;	
+    }
+
+    public static int sub(int a, int b){
+	return 0;	
+    }
+    
+    public static int mul(int a, int b){
+	return 0;	
+    }
+    
+    public static int div(int a, int b){
+	return 0;	
+    }
+
+    public static int mod(int a, int b){
+	return 0;	
+    }
+}
