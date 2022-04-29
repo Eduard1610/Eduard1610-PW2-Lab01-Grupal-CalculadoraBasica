3f2cca4a817140c6b90812c6373e6b00367f44a2
JChua21 <jchua@unsa.edu.pe>
2022-04-29 18:39:33 -0500

    Se añadio entrada del usuario, operacion módulo y las impresiones

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main (String[] args) {
+	Scanner scan = new Scanner(System.in);
+	int a, b;
+	 System.out.println("========= CALCULADORA ========");
+	 System.out.print("Ingrese a : ");
+	 a = scan.nextInt();
 
+	 System.out.print("Ingrese b : ");
+	 b = scan.nextInt();
+
+
+	 System.out.println("La suma de " + a  + " + " +  b + " es  : " + add(a,b) );
+	 System.out.println("La resta de " + a  + " - " +  b + " es  : " + sub(a,b) );
+	 System.out.println("La multiplicaci√n de " + a  + " * " +  b + " es  : " + mul(a,b) );
+	 System.out.println("La divisi√n de " + a  + " / " +  b + " es  : " + div(a,b) );
+	 System.out.println("El m√≥dulo de " + a  + " % " +  b + " es  : " +mod(a,b) );
     }
 	
     public static int add(int a, int b){
@@ -21,6 +37,8 @@ public class Calculator {
     }
 
     public static int mod(int a, int b){
-	return 0;	
+	int mod = a % b;
+	return mod;	
+
     }
 }
